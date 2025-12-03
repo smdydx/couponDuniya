1e59a5e5856380d61179e48f9acac0a2e1ef8589
smdydx <alamsamad36@gmail.com>
2025-12-03 15:53:57 +0530

    update

--- backend/app/models/product.py
+++ backend/app/models/product.py
@@ -17,5 +17,6 @@ class Product(Base):
     is_featured: Mapped[bool] = mapped_column(Boolean, default=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    variants = relationship("ProductVariant", back_populates="product")
 
     merchant = relationship("Merchant")
--- backend/app/models/product_variant.py
+++ backend/app/models/product_variant.py
@@ -1,5 +1,5 @@
-from sqlalchemy import String, Integer, ForeignKey, Numeric
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer, ForeignKey, Numeric ,Boolean
+from sqlalchemy.orm import Mapped, mapped_column,relationship
 from ..database import Base
 
 class ProductVariant(Base):
@@ -11,3 +11,6 @@ class ProductVariant(Base):
     name: Mapped[str] = mapped_column(String(255))
     price: Mapped[float] = mapped_column(Numeric(10,2))
     stock: Mapped[int] = mapped_column(Integer, default=0)
+    product = relationship("Product", back_populates="variants")
+    is_available: Mapped[bool] = mapped_column(Boolean, default=True)
+
