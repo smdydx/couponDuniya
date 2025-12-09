bd4f2e8931a400e77369d2e757b1c1c57c70b0bf
Samad Alam <77625439+smdydx@users.noreply.github.com>
2025-12-09 12:27:09 +0000

    update

---
+++ backend/alembic/versions/432203343fa4_add_user_profile_fields.py
@@ -0,0 +1,17 @@
+"""add_user_profile_fields"""
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '432203343fa4'
+down_revision = '895745d63e93'
+branch_labels = None
+depends_on = None
+
+def upgrade() -> None:
+    pass
+
+def downgrade() -> None:
+    pass
+
